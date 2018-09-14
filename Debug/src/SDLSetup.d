5a7b2b2f28f4996056f2a30ea6356b442bf56c6f
KS <salkam3156@hotmail.com>
2018-09-14 17:42:19 +0000

    rm debug

--- Debug/src/SDLSetup.d
+++
@@ -1,8 +0,0 @@
-src/SDLSetup.d: ../src/SDLSetup.cpp ../src/SDLSetup.h \
- ../src/StandardHeader.h ../src/Interfaces/IInitializer.h
-
-../src/SDLSetup.h:
-
-../src/StandardHeader.h:
-
-../src/Interfaces/IInitializer.h:
--- Debug/src/Window.d
+++
@@ -1,5 +0,0 @@
-src/Window.d: ../src/Window.cpp ../src/Window.h ../src/StandardHeader.h
-
-../src/Window.h:
-
-../src/StandardHeader.h:
