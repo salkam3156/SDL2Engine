aab393221a13361e35e55316aed08309ddd2b921
tushwintz <salkam3156@hotmail.com>
2018-10-17 19:38:55 +0200

    removing stride-specific stuff from Shape class. Color data = vert data for now

---
+++ src/Types/Shape.cpp
@@ -0,0 +1,11 @@
+#include "Shape.h"
+
+Shape::Shape(std::shared_ptr<ShaderProgram> shader) : DrawableObject()
+{
+	LoadVertices(_cube.GetVertices());
+	LoadElements(_cube.GetElements());
+
+	glEnableVertexAttribArray(shader->GetPositionAttrLocation());
+	//TODO: make stride a member of the struct / implement shape/resource maker
+	glVertexAttribPointer(shader->GetPositionAttrLocation(), 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), 0);
+}
---
+++ src/Types/Shape.h
@@ -0,0 +1,20 @@
+#pragma once
+#include "../StandardHeader.h"
+#include "../Shaders/ShaderProgram.h"
+#include "DrawableObject.h"
+#include "../../res/Quad.h"
+#include "../../res/Cube.h"
+
+class Shape : public DrawableObject
+{
+public:
+	Shape(std::shared_ptr<ShaderProgram> shader);
+	~Shape() {}
+
+
+private:
+	GLint _positionAttr;
+	GLint _colorAttr;
+	Quad _quad;
+	Cube _cube;
+};
