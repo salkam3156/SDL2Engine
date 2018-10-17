c50e9522196fd6937d9db18366053c67ec09e116
tushwintz <salkam3156@hotmail.com>
2018-10-17 19:55:44 +0200

    seting depth and stencil size

--- src/Window.cpp
+++ src/Window.cpp
@@ -26,6 +26,8 @@ Window::Window(const std::string& windowTitle) :
 	SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
 	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
 	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3);
+	SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
+	SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 8);
 
 	InitWindow(windowTitle);
 	InitContext();
